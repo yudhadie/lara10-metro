05beb5f66bbe144caef1b5a7f6de1ae94a1b2444
yudhadie <yudha.tp3@gmail.com>
2023-02-17 14:14:31 +0700

    add admin seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
+            $table->foreignId('current_team_id')->default(2);
+            $table->integer('active')->default(1);
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
